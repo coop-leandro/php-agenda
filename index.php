c762f9964b8ce1956a4b018c6af174fc36ba860e
Leandro <leandrocorneo5@unesc.net>
2024-10-25 14:21:35 -0300

    home page

--- index.php
+++ index.php
@@ -1,8 +1,47 @@
 <?php 
     include_once("templates/header.php");
 ?>   
-    
 
+<div class="container">
+    <?php if(isset($printMsg) && $printMsg != ''): ?>
+        <p class="msg"><?= $printMsg ?></p>
+    <?php endif ?>
+    <h1 class="main-title">Minha Agenda</h1>
+    <?php if(count($contacts) > 0): ?>
+        <table class="table contacts-table">
+            <thead class="">
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Nome</th>
+                    <th scope="col">Telefone</th>
+                    <th scope="col"></th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach($contacts as $contact): ?>
+                    <tr>
+                        <td scope="row"><?=$contact['id']?></td>
+                        <td scope="row"><?=$contact['name']?></td>
+                        <td scope="row"><?=$contact['phone']?></td>
+                        <td class="actions">
+                            <a href="#">
+                                <i class="fas fa-eye check-icon"></i>
+                            </a>
+                            <a href="#">
+                                <i class="far fa-edit edit-icon"></i>
+                            </a>
+                            <button type="submit"><i class="fas fa-times delete-icon"></i></button>
+                        </td>
+                    </tr>
+                <?php endforeach ?>
+            </tbody>
+        </table>
+    <?php else: ?>
+        <p class="empty-list-text">Ainda nao ha contatos na sua agenda, 
+            <a href="<?= $BASE_URL ?>create.php">Clique aqui para adicionar contatos</a>
+        </p>
+    <?php endif ?>
+</div>
 
 <?php 
     include_once("templates/footer.php");
