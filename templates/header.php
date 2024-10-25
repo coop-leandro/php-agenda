93b23e9c018d3f6841b3046f917ce075b64aa7f7
Leandro <leandrocorneo5@unesc.net>
2024-10-25 13:44:02 -0300

    add templates

---
+++ templates/header.php
@@ -0,0 +1,28 @@
+<?php 
+    include_once("config/url.php");
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.3/css/bootstrap.min.css" integrity="sha512-jnSuA4Ss2PkkikSOLtYs8BlYIeeIK1h99ty4YfvRPAlzr377vr3CXDb7sb7eEEBYjDtcYj+AjBH3FLv5uSJuXg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <title>Agenda</title>
+    <link rel="stylesheet" href="<?=$BASE_URL?>css/styles.css">
+</head>
+<body>
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+            <a class="navbar-brand" href="<?=$BASE_URL?>index.php">
+                <img src="<?=$BASE_URL?>img/logo.svg" alt="">
+            </a>
+            <div>
+                <div class="navbar-nav">
+                    <a href="<?=$BASE_URL?>index.php" id="home-link" class="nav-link active ">Agenda</a>
+                    <a href="<?=$BASE_URL?>create.php" id="home-link" class="nav-link active">Adicionar Contato</a>
+                </div>
+            </div>
+        </nav>
+    </header>
