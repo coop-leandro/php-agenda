307f5a853ed049bae6c5b94709839286548226fd
Leandro <leandrocorneo5@unesc.net>
2024-10-25 15:18:15 -0300

    view page

--- config/process.php
+++ config/process.php
@@ -4,13 +4,26 @@
     include_once("connection.php");
     include_once("url.php");
 
-    $contacts = [];
+    $id;
 
-    $query = "SELECT * FROM contacts";
+    if(!empty($_GET)){
+        $id = $_GET['id'];
+    }
 
-    $stmt = $conn->prepare($query);
+    if(!empty($id)){
+        $query = "SELECT * FROM contacts WHERE id = :id";
+        $stmt = $conn->prepare($query);
+        $stmt->bindParam(":id", $id);
+        $stmt->execute();
+        $contact = $stmt->fetch(); //retorna o contato com o id selecionado //
+    }else{
+        $contacts = [];
+        $query = "SELECT * FROM contacts";
+        $stmt = $conn->prepare($query);
+        $stmt->execute();
+        $contacts = $stmt->fetchAll(); //busca todas rows da tabela selecionada na query //
+    }
 
-    $stmt->execute();
 
-    $contacts = $stmt->fetchAll(); //busca todas rows da tabela selecionada na query //
+    
 
