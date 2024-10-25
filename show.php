f254b102e8d9b82a7a898251147bc08ac3ef5495
Leandro <leandrocorneo5@unesc.net>
2024-10-25 15:27:42 -0300

    view css

--- show.php
+++ show.php
@@ -1,10 +1,11 @@
 <?php include_once("templates/header.php");?>
 
 <div class="container view-contact-container">
+    <?php include_once("templates/backbutton.html") ?>
     <h1 class="main-title"><?= $contact['name']; ?></h1>
-    <p class="bold"></p>
+    <p class="bold">Telefone:</p>
     <p><?= $contact['phone'] ?></p>
-    <p class="bold">Observações</p>
+    <p class="bold">Observações:</p>
     <p><?= $contact['observation'] ?></p>
 </div>
 
